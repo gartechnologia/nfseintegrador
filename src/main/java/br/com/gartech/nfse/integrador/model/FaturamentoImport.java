ffbb58207bf2480d121402edd40767443efd50d1
Gilson Aparecido Rodrigues <gilson.aparecido.rodrigues@gmail.com>
2016-09-09 18:27:42 -0400

    Incluindo classe para representar a rotina de importação do faturamento mensal

---
+++ src/main/java/br/com/gartech/nfse/integrador/model/FaturamentoImport.java
@@ -0,0 +1,322 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.gartech.nfse.integrador.model;
+
+
+/**
+ *
+ * @author Gilson
+ */
+public class FaturamentoImport {
+    
+    private String campus;
+    private String numero;
+    private String tipo_fatura;
+    private int anocompetencia;
+    private int mescompetencia;
+    private int codaluno;
+    private String alunome;
+    private int codcurso;
+    private String curso;
+    private String tipocurso;
+    private String aluendereco;
+    private String alubairro;
+    private String alucep;
+    private String ciddesc;
+    private String ciduf;
+    private String alutelefone;
+    private String alutelefone2;
+    private String aluemail;
+    private String alucpf;
+    private float valor;
+
+    public FaturamentoImport() {
+    }
+
+    /**
+     * @return the campus
+     */
+    public String getCampus() {
+        return campus;
+    }
+
+    /**
+     * @param campus the campus to set
+     */
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    /**
+     * @return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the tipo_fatura
+     */
+    public String getTipo_fatura() {
+        return tipo_fatura;
+    }
+
+    /**
+     * @param tipo_fatura the tipo_fatura to set
+     */
+    public void setTipo_fatura(String tipo_fatura) {
+        this.tipo_fatura = tipo_fatura;
+    }
+
+    /**
+     * @return the anocompetencia
+     */
+    public int getAnocompetencia() {
+        return anocompetencia;
+    }
+
+    /**
+     * @param anocompetencia the anocompetencia to set
+     */
+    public void setAnocompetencia(int anocompetencia) {
+        this.anocompetencia = anocompetencia;
+    }
+
+    /**
+     * @return the mescompetencia
+     */
+    public int getMescompetencia() {
+        return mescompetencia;
+    }
+
+    /**
+     * @param mescompetencia the mescompetencia to set
+     */
+    public void setMescompetencia(int mescompetencia) {
+        this.mescompetencia = mescompetencia;
+    }
+
+    /**
+     * @return the codaluno
+     */
+    public int getCodaluno() {
+        return codaluno;
+    }
+
+    /**
+     * @param codaluno the codaluno to set
+     */
+    public void setCodaluno(int codaluno) {
+        this.codaluno = codaluno;
+    }
+
+    /**
+     * @return the alunome
+     */
+    public String getAlunome() {
+        return alunome;
+    }
+
+    /**
+     * @param alunome the alunome to set
+     */
+    public void setAlunome(String alunome) {
+        this.alunome = alunome;
+    }
+
+    /**
+     * @return the codcurso
+     */
+    public int getCodcurso() {
+        return codcurso;
+    }
+
+    /**
+     * @param codcurso the codcurso to set
+     */
+    public void setCodcurso(int codcurso) {
+        this.codcurso = codcurso;
+    }
+
+    /**
+     * @return the curso
+     */
+    public String getCurso() {
+        return curso;
+    }
+
+    /**
+     * @param curso the curso to set
+     */
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    /**
+     * @return the tipocurso
+     */
+    public String getTipocurso() {
+        return tipocurso;
+    }
+
+    /**
+     * @param tipocurso the tipocurso to set
+     */
+    public void setTipocurso(String tipocurso) {
+        this.tipocurso = tipocurso;
+    }
+
+    /**
+     * @return the aluendereco
+     */
+    public String getAluendereco() {
+        return aluendereco;
+    }
+
+    /**
+     * @param aluendereco the aluendereco to set
+     */
+    public void setAluendereco(String aluendereco) {
+        this.aluendereco = aluendereco;
+    }
+
+    /**
+     * @return the alubairro
+     */
+    public String getAlubairro() {
+        return alubairro;
+    }
+
+    /**
+     * @param alubairro the alubairro to set
+     */
+    public void setAlubairro(String alubairro) {
+        this.alubairro = alubairro;
+    }
+
+    /**
+     * @return the alucep
+     */
+    public String getAlucep() {
+        return alucep;
+    }
+
+    /**
+     * @param alucep the alucep to set
+     */
+    public void setAlucep(String alucep) {
+        this.alucep = alucep;
+    }
+
+    /**
+     * @return the ciddesc
+     */
+    public String getCiddesc() {
+        return ciddesc;
+    }
+
+    /**
+     * @param ciddesc the ciddesc to set
+     */
+    public void setCiddesc(String ciddesc) {
+        this.ciddesc = ciddesc;
+    }
+
+    /**
+     * @return the ciduf
+     */
+    public String getCiduf() {
+        return ciduf;
+    }
+
+    /**
+     * @param ciduf the ciduf to set
+     */
+    public void setCiduf(String ciduf) {
+        this.ciduf = ciduf;
+    }
+
+    /**
+     * @return the alutelefone
+     */
+    public String getAlutelefone() {
+        return alutelefone;
+    }
+
+    /**
+     * @param alutelefone the alutelefone to set
+     */
+    public void setAlutelefone(String alutelefone) {
+        this.alutelefone = alutelefone;
+    }
+
+    /**
+     * @return the alutelefone2
+     */
+    public String getAlutelefone2() {
+        return alutelefone2;
+    }
+
+    /**
+     * @param alutelefone2 the alutelefone2 to set
+     */
+    public void setAlutelefone2(String alutelefone2) {
+        this.alutelefone2 = alutelefone2;
+    }
+
+    /**
+     * @return the aluemail
+     */
+    public String getAluemail() {
+        return aluemail;
+    }
+
+    /**
+     * @param aluemail the aluemail to set
+     */
+    public void setAluemail(String aluemail) {
+        this.aluemail = aluemail;
+    }
+
+    /**
+     * @return the alucpf
+     */
+    public String getAlucpf() {
+        return alucpf;
+    }
+
+    /**
+     * @param alucpf the alucpf to set
+     */
+    public void setAlucpf(String alucpf) {
+        this.alucpf = alucpf;
+    }
+
+    /**
+     * @return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+
+
+    
+}
